def1eab57040dd9e315ad7a28dedd908ab47dd8f
Siva prasad <sivaprasad3150@gmail.com>
2025-06-19 00:34:38 +0530

    user-profile

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "cidr_block" {
-    type = list(string)
-    default = ["10.0.0.0/16"]
+    default = "10.0.0.0/16"
 }
 
 # variable "common_tags" {
@@ -12,28 +11,83 @@ variable "cidr_block" {
 # }
 
 variable "project_name" {
-
+ type = string
 }
 
 variable "environment" {
-
+ type = string
 }
 
-variable "tags" {
+variable "vpc_tags" {
     type = map(string)
+    default = {}
+}
+
+variable "igw_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "public_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "database_subnet_tags" {
+    type = map(string)
+    default = {}
 }
 
 variable "public_subnet_cidrs" {
     type = list(string)
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    #default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
     type = list(string)
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+    #default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
     type = list(string)
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
+    #default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "eip_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "databse_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "is_peering_required" {
+    default = false
+}
+
+variable "peering_tags" {
+    type = map(string)
+    default = {}
 }
