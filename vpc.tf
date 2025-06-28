9424928026847c3a16abce2ff94bbbeef05cd4bd
Siva prasad <sivaprasad3150@gmail.com>
2025-06-28 16:14:46 +0530

    user-profile

--- vpc.tf
+++ vpc.tf
@@ -51,7 +51,7 @@ resource "aws_subnet" "private" {
     count = length(var.private_subnet_cidrs)
     vpc_id = aws_vpc.main.id 
     cidr_block = var.private_subnet_cidrs[count.index]
-    
+    availability_zone = local.az_names[count.index]
     tags = merge(
         var.private_subnet_tags,
         local.common_tags,
